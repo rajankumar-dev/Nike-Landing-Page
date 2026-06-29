99854d93d0838370457311453d205dd7048fbc24
rajankumar-dev <rk1620703@gmail.com>
2026-06-29 16:12:01 +0530

    Convert basic structure into components based layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div >
-        <nav className='flex justify-between mt-5 max-w-7xl col my-0 mx-auto h-72px'>
-          <div className="logo">
-            <img src="brand_logo.png" alt="logo" />
-          </div>
-          <div>
-            <ul className='flex justify-between gap-10'>
-              <li href="#">MENU</li>
-              <li href="#">LOCATION</li>
-              <li href="#">ABOUT</li>
-              <li href="#">CONTACT</li>
-            </ul>
-          </div>
-          <div className="login">
-            <button className='bg-[#D01C28] text-[#fff] px-4 py-1 rounded-lg'>
-              Login
-            </button>
-          </div>
-        </nav>
-      </div>
+      <Navbar />
+      <Home />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+    return (
+        <nav className='flex justify-between mt-5 max-w-7xl col my-0 mx-auto h-72px'>
+            <div className="logo">
+                <img src="brand_logo.png" alt="logo" />
+            </div>
+            <div>
+                <ul className='flex justify-between gap-10'>
+                    <li href="#">MENU</li>
+                    <li href="#">LOCATION</li>
+                    <li href="#">ABOUT</li>
+                    <li href="#">CONTACT</li>
+                </ul>
+            </div>
+            <div className="login">
+                <button className='bg-[#D01C28] text-[#fff] px-4 py-1 rounded-lg'>
+                    Login
+                </button>
+            </div>
+        </nav>
+    );
+}
